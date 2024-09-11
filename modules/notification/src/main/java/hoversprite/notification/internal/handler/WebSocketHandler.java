1b8d69303ea2ff0efde1ac986f5c2732f6cd62ba
phucdoan2003 <thienphucdoantran@gmail.com>
2024-09-11 21:55:36 +0700

    added notification module

---
+++ modules/notification/src/main/java/hoversprite/notification/internal/handler/WebSocketHandler.java
@@ -0,0 +1,8 @@
+package hoversprite.notification.internal.handler;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.socket.handler.TextWebSocketHandler;
+
+@Component
+public class WebSocketHandler extends TextWebSocketHandler {
+}
