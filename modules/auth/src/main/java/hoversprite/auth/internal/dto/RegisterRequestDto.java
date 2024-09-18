50cd84457cbd539411c13ef5305bb441285b4886
phucdoan2003 <thienphucdoantran@gmail.com>
2024-09-18 13:44:31 +0700

    validation for password and fullname

--- modules/auth/src/main/java/hoversprite/auth/internal/dto/RegisterRequestDto.java
+++ modules/auth/src/main/java/hoversprite/auth/internal/dto/RegisterRequestDto.java
@@ -12,7 +12,7 @@ import jakarta.validation.constraints.Pattern;
 @NoArgsConstructor
 public class RegisterRequestDto {
 
-    @Pattern(regexp = "^(?!.*[A-Z]{2})(?=.*\\b([A-Za-z]*[A-Z][a-z]*){1,2}\\b)[A-Za-z ]+$", message = "Each word cannot have more than 2 capitalized letters and they cannot be next to each other. The name must contain only letters and spaces.")
+    @Pattern(regexp = "^(?=\\b[A-Za-z]*[A-Z][a-z]*\\b)(?!.*[A-Z]{2})[A-Za-z ]+$", message = "Name must contain at least one capital letter, no two consecutive capital letters, and consist of only letters and spaces.")
     private String fullName;
 
     @Pattern(regexp = "^((\\+84\\s\\d{3}\\s\\d{3}\\s\\d{3})|(0\\d{3}\\s\\d{3}\\s\\d{3})|((\\+84|0)\\d{9}))$", message = "Phone number must start with +84 or 0. If it starts with +84, it should be formatted as +84 888 888 888. If it starts with 0, it should be formatted as 0888 888 888. Otherwise, no spaces should be included.")
@@ -34,7 +34,7 @@ public class RegisterRequestDto {
 
     private Expertise expertise;
 
-    @Pattern(regexp = "^(?=.*[0-9])(?=.*[!@#$%^&*(),.?\":{}|<>])[A-Za-z0-9!@#$%^&*(),.?\":{}|<>]{6,}$", message = "Password must be at least 6 characters long, contain at least 1 number, and 1 special character")
+    @Pattern(regexp = "^(?=.*[A-Z])(?=.*[!@#$%^&*(),.?\":{}|<>])[A-Za-z0-9!@#$%^&*(),.?\":{}|<>]{6,}$", message = "Password must be at least 6 characters long, contain at least one uppercase letter, and one special character.")
     private String password;
 
 }
